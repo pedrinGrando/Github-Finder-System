dce08da7ab79a3ea9d00ebf9e83414b6f3865263
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2024-03-07 15:06:43 -0300

    Teste

--- Github-finder/github_finder/src/App.tsx
+++ Github-finder/github_finder/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { Outlet } from "react-router-dom"
+
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
           GitHub Finder
+          <Outlet/>
       </div>
     </>
   )
