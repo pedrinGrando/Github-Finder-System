6679c0e5ec843a7e8538c95654c2d346c12cc7f0
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2024-03-11 13:54:04 -0300

    Adição de componentes de Busca

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <div>
+          GitHub Finder
+          <Outlet/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+//pages
+import Home from './routes/Home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
